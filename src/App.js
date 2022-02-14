f40c747965671105da6ad0bd8323a18b75020110
Rakibul islam <himdrakibulislam@gmail.com>
2022-02-14 14:01:26 +0600

    addeddd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
-
+import OrderReview from './components/OrderReview/OrderReview';
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
+      <OrderReview></OrderReview>
     </div>
   );
 }
